002a2ecd3b11947b0b09288b82e1e5c94c0a2332
devjanaprime <dev@primeacademy.io>
2017-05-12 09:24:42 -0500

    removed db stuff

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ var express = require( 'express' );
 var app = express();
 var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
-var mongoose = require( 'mongoose' );
 var index = require( './modules/index' );
 
 var port = process.env.PORT || 6789;
